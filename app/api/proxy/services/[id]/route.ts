a80ba7cb5e36fe039250662559dda02dfdf69b40
Yusufbek9 <ticho350@gmail.com>
2025-06-02 19:08:54 +0500

    fixing errors

--- app/api/proxy/services/[id]/route.ts
+++ app/api/proxy/services/[id]/route.ts
@@ -1,10 +1,10 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(
-    request: Request,
-    { params }: { params: { id: string } },
+    request: NextRequest,
+    context: { params: { id: string } },
 ) {
-    const { id } = params;
+    const { id } = context.params;
     try {
         const res = await fetch(
             `https://kasimov.repid.uz/api/v1/common/services/${id}/`,
